88bd09a42a49b63bc820f36cce3299be8c0e6c3f
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-05-11 21:13:42 +0500

    Setup hero section generator function

--- src/home-page.js
+++ src/home-page.js
@@ -2,8 +2,11 @@ export default () => {
   const content = document.getElementById("content");
   console.log(content);
 
-  const header = generateHeader(content);
+  const header = generateHeader();
+  const hero = generateHeroSection();
+
   content.appendChild(header);
+  content.appendChild(hero);
 };
 
 function generateHeader() {
@@ -14,11 +17,13 @@ function generateHeader() {
 
   const pageIDs = ["home", "about-us", "menu"];
   for (let id of pageIDs) {
-    const li = document.createElement("li");
     const button = document.createElement("button");
     button.id = id;
     button.textContent = cleanName(id);
+
+    const li = document.createElement("li");
     li.appendChild(button);
+
     ul.appendChild(li);
   }
 
@@ -33,6 +38,37 @@ function generateHeader() {
   header.appendChild(hr);
 
   return header;
+}
+
+function generateHeroSection() {
+  const section = document.createElement("section");
+  section.id = "hero";
+
+  const left = document.createElement("div");
+  left.classList.add("left");
+  const right = document.createElement("div");
+  left.classList.add("right");
+
+  const h2 = document.createElement("h2");
+  h2.textContent = "A heaven for lovers of Georgia";
+  left.appendChild(h2);
+
+  const p = document.createElement("p");
+  p.textContent = `You can try out dozens of different georgian dishes in our 
+  restaurant by democratic prices. What are you even waiting for?`;
+  left.appendChild(p);
+
+  const callToActionButton = document.createElement("button");
+  callToActionButton.textContent = "Book a table!";
+  left.appendChild(callToActionButton);
+
+  const img = document.createElement("img");
+  img.setAttribute("alt", "placeholder");
+  right.appendChild(img);
+
+  section.appendChild(left);
+  section.appendChild(right);
+  return section;
 }
 
 function cleanName(name) {
