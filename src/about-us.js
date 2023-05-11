4b7dfa112c6ad468dff912fc035b5cdbfdecd84c
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-05-11 22:02:00 +0500

    Setup about us page layout

--- src/about-us.js
+++ src/about-us.js
@@ -1,3 +1,31 @@
 export default function (content) {
-  console.log("Something!");
+  const h2 = document.createElement("h2");
+  h2.textContent = "About us";
+
+  const main = document.createElement("main");
+
+  const left = document.createElement("div");
+  left.classList.add("left");
+
+  const p = document.createElement("p");
+  p.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Ipsum accusantium ipsam modi eius. Temporibus iure distinctio numquam 
+  recusandae eos a aperiam autem? Nostrum, reiciendis. Doloribus, hic harum. 
+  Commodi sint tenetur magnam exercitationem, debitis quod excepturi porro, 
+  ipsam voluptatem ex et voluptate doloribus ipsa repellendus repudiandae ad 
+  eaque temporibus maxime ea.`;
+  left.appendChild(p);
+
+  const right = document.createElement("div");
+  right.classList.add("right");
+
+  const img = document.createElement("img");
+  img.setAttribute("alt", "placeholder");
+  right.appendChild(img);
+
+  main.appendChild(left);
+  main.appendChild(right);
+
+  content.appendChild(h2);
+  content.appendChild(main);
 }
