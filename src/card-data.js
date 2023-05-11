f720055bcdc386c09faf53d87c200a5f9f81f40f
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-05-11 22:21:17 +0500

    Setup menu page layout

--- src/card-data.js
+++ src/card-data.js
@@ -23,4 +23,52 @@ export default {
     title: "WOAH",
     description: "This is a random description of some random bootleg dish.",
   },
+  3: {
+    img: {
+      alt: "placeholder",
+      link: ".",
+    },
+    title: "WOAH",
+    description: "This is a random description of some random bootleg dish.",
+  },
+  4: {
+    img: {
+      alt: "placeholder",
+      link: ".",
+    },
+    title: "WOAH",
+    description: "This is a random description of some random bootleg dish.",
+  },
+  5: {
+    img: {
+      alt: "placeholder",
+      link: ".",
+    },
+    title: "WOAH",
+    description: "This is a random description of some random bootleg dish.",
+  },
+  6: {
+    img: {
+      alt: "placeholder",
+      link: ".",
+    },
+    title: "WOAH",
+    description: "This is a random description of some random bootleg dish.",
+  },
+  7: {
+    img: {
+      alt: "placeholder",
+      link: ".",
+    },
+    title: "WOAH",
+    description: "This is a random description of some random bootleg dish.",
+  },
+  8: {
+    img: {
+      alt: "placeholder",
+      link: ".",
+    },
+    title: "WOAH",
+    description: "This is a random description of some random bootleg dish.",
+  },
 };
--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,36 @@
+import cardData from "./card-data";
+
 export default function (content) {
-  console.log("Something!");
+  const h2 = document.createElement("h2");
+  h2.textContent = "Menu";
+
+  const description = document.createElement("p");
+  description.textContent = `These are our most beloved dishes of all time. 
+  Don't hesitate to try them out yourself!`;
+
+  const menu = document.createElement("section");
+  for (let data of Object.values(cardData)) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    menu.appendChild(card);
+
+    const img = document.createElement("img");
+    img.setAttribute("alt", data.img.alt);
+    card.appendChild(img);
+
+    const hr = document.createElement("hr");
+    card.appendChild(hr);
+
+    const h3 = document.createElement("h3");
+    h3.textContent = data.title;
+    card.appendChild(h3);
+
+    const p = document.createElement("p");
+    p.textContent = data.description;
+    card.appendChild(p);
+  }
+
+  content.appendChild(h2);
+  content.appendChild(description);
+  content.appendChild(menu);
 }
